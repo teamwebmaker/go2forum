51c9c5737b1daa9a610057f60158a014f6c8ed56
Bekauridev <beqaurigiorgi31@gmail.com>
2026-03-18 11:16:30 +0400

    small ui update

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -6,7 +6,7 @@
             ? route('categories.topics', $topic->category)
             : route('page.home');
     @endphp
-    <div class="mx-auto flex w-full max-w-5xl flex-col gap-6 px-4 sm:px-6 lg:px-0">
+    <div class="mx-auto flex w-full max-w-5xl flex-col gap-6 md:px-6 lg:px-0">
         <a href="{{ $backUrl }}"
             class="inline-flex items-center gap-1 text-sm font-medium text-slate-600 transition hover:text-slate-900">
             <x-app-icon name="chevron-left" class="h-4 w-4" />
